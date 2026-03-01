d36456206dbb47be580b8c0c359d035d42815e85
dakychan <kotaychinkot4@gmail.com>
2026-03-01 21:34:43 +0500

    Coklin, kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,84 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "2.3.10"
+    id("fabric-loom") version "1.16.0-alpha.10"
+    id("maven-publish")
+    id("org.jetbrains.kotlin.plugin.lombok") version "2.3.20-RC"
+}
+
+version = project.property("mod_version").toString()
+group = project.property("maven_group").toString()
+
+base {
+    archivesName.set(project.property("archives_base_name").toString())
+}
+
+val targetJavaVersion = 25
+
+repositories {
+    maven("https://jitpack.io")
+    maven("https://maven.maxhenkel.de/repository/public")
+}
+
+loom {
+    accessWidenerPath.set(file("src/main/resources/accesswidener"))
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+    mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+    modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("kotlin_loader_version")}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
+    include(modImplementation("io.netty:netty-handler-proxy:4.1.82.Final")!!)
+    include(modImplementation("io.netty:netty-codec-socks:4.1.82.Final")!!)
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    modCompileOnly("de.maxhenkel.voicechat:voicechat-api:2.6.0")
+    implementation("ai.catboost:catboost-common:1.2.10")
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    inputs.property("minecraft_version", project.property("minecraft_version"))
+    inputs.property("loader_version", project.property("loader_version"))
+    filteringCharset = "UTF-8"
+    filesMatching("fabric.mod.json") {
+        expand(
+            mapOf(
+                "version" to project.version,
+                "minecraft_version" to project.property("minecraft_version"),
+                "loader_version" to project.property("loader_version"),
+                "kotlin_loader_version" to project.property("kotlin_loader_version")
+            )
+        )
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(targetJavaVersion)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions.jvmTarget.set(JvmTarget.fromTarget(targetJavaVersion.toString()))
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename { "${it}_${project.base.archivesName.get()}" }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = project.property("archives_base_name") as String
+            from(components["java"])
+        }
+    }
+    repositories {
+    }
+}
