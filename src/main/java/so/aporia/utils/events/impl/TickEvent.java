8bece4bbf33846989c41d9417a66adce67c97178
dakychan <kotaychinkot4@gmail.com>
2026-03-24 18:38:44 +0500

    2 part of upgrading

--- src/main/java/so/aporia/utils/events/impl/TickEvent.java
+++
@@ -1,9 +0,0 @@
-package so.aporia.utils.events.impl;
-
-/**
- * Fired every game tick - used for server-side logic like AutoFlyMe.
- */
-public final class TickEvent {
-    
-    public TickEvent() {}
-}
